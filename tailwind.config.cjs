16aa70042f475c700431cbb337ae535e3e14e9b1
Joseph Lozano <me@josephlozano.xyz>
2022-10-13 13:42:35 -0400

    fix elint warning on taliwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
 const corporateTheme = require("daisyui/src/colors/themes")[
